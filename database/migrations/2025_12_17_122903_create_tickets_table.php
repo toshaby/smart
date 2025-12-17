6711b96071bf6a3468f0643f7944308db667a2fa
toha <toshaby2@gmail.com>
2025-12-17 15:26:20 +0200

    created migrations and loaded spatie/laravel-medialibrary

---
+++ database/migrations/2025_12_17_122903_create_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('customer_id');
+            $table->string('theme');
+            $table->text('text');
+            $table->enum('status', ['new', 'working', 'processed']);
+            $table->timestamp('answered_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get('/test', function(){
+    //Storage::disk('public')->put('example.txt', 'my new file surok');
+    return asset('storage/example.txt');
+});
+*/
